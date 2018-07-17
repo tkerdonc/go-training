2264fb08860b93ee57a6731f066f597765609d37
Tanguy Kerdoncuff <t.kerdonc@gmail.com>
2018-07-17 17:31:02 +0200

    Acronym solution

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -1,15 +1,34 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// This package implements the core functionnality of communication
+// sciences, transforming words into acronyms
 package acronym
 
-// Abbreviate should have a comment documenting it.
-func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+import "strings"
+
+// Generates and acronym our of a string
+// parameters
+// * inputString : base string for the acronym. Will be split into words
+// 				   using ' ' or '-' as a separator. Each word will then
+//				   be used to compute the returned acronym
+func Abbreviate(inputString string) string {
+	// Who knows when we would like another separator...
+	separators := [2]string{" ", "-"}
+	acronym := ""
+	words := []string{inputString}
+	splittedWords :=[]string{}
+
+	// Iterate the separators and words, creating a flattened list
+	for _, separator := range(separators) {
+		splittedWords =[]string{}
+		for _, word := range(words) {
+			splittedWords = append(splittedWords, strings.Split(word, separator)...)
+		}
+		words = splittedWords
+	}
+
+	// Pick the first letter
+	for _, word := range(words) {
+		acronym += (word[:1])
+	}
+
+	return strings.ToUpper(acronym)
 }
