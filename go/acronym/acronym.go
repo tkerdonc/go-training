967d5c79689fe23b00df14bddd77551ad6edad03
Tanguy Kerdoncuff <t.kerdonc@gmail.com>
2018-07-18 17:51:33 +0200

    Integrating final remarks in acronym excercise

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -6,8 +6,8 @@ import "strings"
 
 // Abbreviate generates an acronym representing a string parameter.
 // * inputString : base string for the acronym. Will be split into words
-//				   using ' ' or '-' as a separator. Each word will then
-//                 be used to compute the returned acronym
+//					using ' ' or '-' as a separator. Each word will then
+//					be used to compute the returned acronym
 func Abbreviate(inputString string) string {
 	// Who knows when we would like another separator...
 	separators := map[rune]struct{}{' ': {}, '-': {}}
@@ -19,7 +19,6 @@ func Abbreviate(inputString string) string {
 		if _, isSeparator := separators[lastRune]; isSeparator {
 			// Check that current rune is not a separator
 			if _, isSeparator := separators[currentRune]; !isSeparator {
-				acronymBuilder.Grow(4) // Rune size would be 4 bytes right ?
 				acronymBuilder.WriteRune(currentRune)
 			}
 		}
