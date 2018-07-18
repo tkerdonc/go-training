f13c451d248ce97cdd6920442f07be8ff0863413
Tanguy Kerdoncuff <t.kerdonc@gmail.com>
2018-07-18 14:23:13 +0200

    Solution to hamming excercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,37 @@
+// This package implements an hamming distance calculation function
+// aimed at DNA strands
+
 package hamming
 
-func Distance(a, b string) (int, error) {
+import (
+	"errors"
+	"strings"
+)
+
+/*  Distance returns the hamming distance between two dna strands
+    passed as string parameters. Returns -1 if the strands are of
+	different lengths.
+    parameters :
+		a : strand as a string
+		b : strand as a string
+*/
+func Distance(a, b string) (distance int, distanceError error) {
+	if len(a) != len(b) {
+		distance = -1
+		distanceError = errors.New("Distance expects strands to be of equal lengths.")
+	} else {
+		distanceError = nil
+		distance = 0
+		var aReader = strings.NewReader(a)
+		var bReader = strings.NewReader(b)
+		aRune, _, aErr := aReader.ReadRune()
+		for aErr == nil {
+			bRune, _, _ := bReader.ReadRune()
+			if aRune != bRune {
+				distance += 1
+			}
+			aRune, _, aErr = aReader.ReadRune()
+		}
+	}
+	return
 }
